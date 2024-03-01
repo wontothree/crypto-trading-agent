d8b20a9093eefa60970cf2a327791d6f2d3b506c
williamkevin <prepared7913@naver.com>
2024-03-01 16:54:31 +0900

    feat : add description for function and variable

--- classes.py
+++ classes.py
@@ -1,22 +1,43 @@
 import ccxt
+from datetime import datetime
 
 class Myupbit():
+
     # 생성자
     def __init__(self, access, secret):
-        print("딱 한 번만 실행된다.")
+        """
+        Parameters
+        ----------
+        access : access key
+        secret : secret key
+        is_buy : 포지션을 결정하는 스위칭 역할 변수
+        """
         self.upbit = ccxt.upbit()
         self.upbit.apiKey = access
         self.upbit.secret = secret
         self.is_buy = False
 
+
     def watch_and_order(self, symbol, money, watch_percent):
-        print("가격을 감시하다가 매수를 하는 함수")
+        """
+        가격을 감시하다가 매수를 하는 함수
+
+        Parameters
+        ----------
+        symbol : coin.
+            ex) BTC/KRW, DOGE/KRW, TRX/KRW, XRP/KRW.
+        money : 매수 금액.
+        watch_percent : 구매하길 바라는 퍼센트 하락.
+        """
+
         infos = self.upbit.fetch_ticker(symbol)
-        # print(infos)
         price = infos["close"]
-        percent = infos["percentage"] * 100
-        rounded_percent = round(percent, 2)
-        print(f"현재 {symbol}의 가격은 {price}원이고 전일대비 {rounded_percent}% 입니다!")
+        percent = infos["percentage"] * 100 # 백분율
+        rounded_percent = round(percent, 2) # 소수점 두 자리에서 반올림
+        
+        current_time = datetime.now()
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{formatted_time } | {symbol}의 가격은 {price}원이고, 전일대비 {rounded_percent}%입니다!")
 
         if percent < watch_percent and not self.is_buy:
             self.upbit.create_market_order(symbol, "buy", money, 1)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from classes import Myupbit
 
+access_key = "TQcHNv10q7AG2FxSkEhadyQNmf7JEvVmhmoTqQzV"
+secret_key = "Pywp6HfRHcXqoRGcJRQZKF9C2FTGBekCZWQpKk0R"
+
+sewon_upbit = Myupbit(access_key, secret_key)
+
 # 시장가 주문
 # upbit.create_market_order("TRX/KRW", "buy", 10000, 1) # 10,000 x 1 원 어치
 # upbit.create_market_order("TRX/KRW", "sell", ) # 주문수량
@@ -8,16 +13,8 @@ from classes import Myupbit
 # upbit.create_limit_order("TRX/KRW", "buy", 40, 152) # 수량 - 금액
 # upbit.create_limit_order("TRX/KRW", "sell", , ) # 수량 - 금액
 
-
-access_key = "TQcHNv10q7AG2FxSkEhadyQNmf7JEvVmhmoTqQzV"
-secret_key = "Pywp6HfRHcXqoRGcJRQZKF9C2FTGBekCZWQpKk0R"
-
-sewon_upbit = Myupbit(access_key, secret_key)
-
 while True:
-    sewon_upbit.watch_and_order("TRX/KRW", 10000, 0)
-    sewon_upbit.watch_and_order("BTC/KRW", 10000, 0)
-    sewon_upbit.watch_and_order("XRP/KRW", 10000, 0)
+    sewon_upbit.watch_and_order("BTC/KRW", 100000, 0)
 
 
 
