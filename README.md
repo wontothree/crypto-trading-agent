73a716826dd4ac57671c2c26344f47f26c2d911c
williamkevin <prepared7913@naver.com>
2024-03-01 21:41:07 +0900

    feat : discord bot for report

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Auto-trading algorithm
 - 매수하는 함수
 - 매도하는 함수
 
+## Monitoring
+
+모든 코인의 가격을 실시간으로 모니터링한다.
+
+## Reporting
--- observe.py
+++ observe.py
@@ -8,7 +8,7 @@ class Observe():
 
     def observe(self, symbol):
         """
-        가격을 감시하다가 매수를 하는 함수
+        가격을 감시하는 함수
 
         Parameters
         ----------
@@ -18,12 +18,15 @@ class Observe():
 
         infos = self.upbit.fetch_ticker(symbol)
         price = infos["close"]
+        formatted_price = "{:,}".format(price)
+
         percent = infos["percentage"] * 100 # 백분율
         rounded_percent = round(percent, 3) # 소수점 두 자리에서 반올림
+        formatted_percent = f"{rounded_percent:.3f}"
 
         current_time = datetime.now()
         formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-        print(f"| {formatted_time} | {symbol} | 현재가(원) : {price} | 전일 대비 상승률(%) : {rounded_percent} |")
+        print(f"| {formatted_time} | {symbol} | 현재가(원) : {formatted_price} | 전일 대비 상승률(%) : {formatted_percent} |")
 
 
